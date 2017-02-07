757683ee79f68381a055da1189e2714cda1c21e0
Marqus Landin <marqus.landin@boldit.se>
2017-02-07 11:17:48 +0100

    Create az-create-extendedevent-monitor-deadlock-session.sql
    Create an Azure SQL Extended event to monitor deadlocks

---
+++ az-create-extendedevent-monitor-deadlock-session.sql
@@ -0,0 +1,27 @@
+--https://msdn.microsoft.com/en-us/library/dn270018.aspx#Deadlock
+
+IF EXISTS  
+    (SELECT * from sys.database_event_sessions  
+        WHERE name = 'azure_monitor_deadlock_session')  
+BEGIN  
+    ALTER EVENT SESSION azure_monitor_deadlock_session  
+        ON DATABASE  
+        DROP TARGET package0.ring_buffer;  
+  
+    DROP EVENT SESSION azure_monitor_deadlock_session  
+        ON DATABASE;  
+END  
+  
+CREATE EVENT SESSION azure_monitor_deadlock_session  
+    ON DATABASE  
+    ADD EVENT sqlserver.database_xml_deadlock_report  
+    ADD TARGET package0.ring_buffer  
+    (  
+        SET max_memory = 2048, max_events_limit = 10  
+    )  
+    WITH (STARTUP_STATE = ON,  
+          EVENT_RETENTION_MODE = ALLOW_SINGLE_EVENT_LOSS);  
+  
+ALTER EVENT SESSION azure_monitor_deadlock_session  
+    ON DATABASE  
+    STATE = START;  
