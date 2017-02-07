3e6cc7eaa70116a166ba13d43907c5f22c8462dd
Marqus Landin <marqus.landin@boldit.se>
2017-02-07 11:15:18 +0100

    Create az-extended-events-find-deadlocks.sql
    Find deadlocks inside Azure using Extended Events

---
+++ az-extended-events-find-deadlocks.sql
@@ -0,0 +1,18 @@
+WITH CTE AS (  
+    SELECT CAST(xet.target_data AS XML)  AS [target_data_XML]  
+        FROM            sys.dm_xe_database_session_targets AS xet  
+             INNER JOIN sys.dm_xe_database_sessions        AS xe  
+                 ON (xe.address = xet.event_session_address)  
+        WHERE xe.name = 'azure_monitor_deadlock_session'  
+)  
+, CTE2 AS (  
+    SELECT  
+            T2.EventData.query('.').value(  
+                '(/event/@timestamp)[1]', 'DateTime2') AS Timestamp,  
+            T2.EventData.query('.').query(  
+                '(/event/data/value/deadlock)[1]')     AS deadlock_xml  
+        FROM CTE  
+            CROSS Apply [target_data_XML].nodes(  
+                '/RingBufferTarget/event') AS T2(EventData)  
+)  
+SELECT * FROM CTE2
