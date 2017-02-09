c8a0a4274db31ee8c8fbcb3979e418bd45a369f9
Marqus Landin <marqus.landin@boldit.se>
2017-02-09 14:01:35 +0100

    Create delete-in-chunks.sql
    If you have a very large number of items that will be deleted that that impacts locking and writing huge amounts of data in a single transaction to the journal log which can result in deadlocks
    
    A better implementation is to do batched deletes in separate transactions until there are no more items to be purged.
    
    Using SQL server this can be done via DELETE top (10000) FROM Table

---
+++ delete-in-chunks.sql
@@ -0,0 +1,19 @@
+SET NOCOUNT ON;
+ 
+DECLARE @r INT;
+ 
+SET @r = 1;
+ 
+WHILE @r > 0
+BEGIN
+  BEGIN TRANSACTION;
+ 
+  DELETE TOP (1000)
+    FROM OutboxRecord 
+	WHERE Dispatched = 1 and DispatchedAt <= DateAdd(DAY, -7, GETUTCDATE());
+ 
+  SET @r = @@ROWCOUNT;
+  Print @r
+ 
+  COMMIT TRANSACTION;
+END
