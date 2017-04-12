650eb928e6cc87ad442ec52207eddc84bceec8a9
Marqus Landin <marqus.landin@boldit.se>
2017-04-12 12:33:22 +0200

    Update create_sql_user.sql

--- create_sql_user.sql
+++ create_sql_user.sql
@@ -1,5 +1,5 @@
 /*1: Create SQL Login on master database (connect with admin account to master database)*/
-CREATE LOGIN xxx WITH PASSWORD = ‘password’;
+CREATE LOGIN xxx WITH PASSWORD = 'password';
 /*2: Create SQL user on the master database (this is necessary for login attempt to the <default> database, as with Azure SQL you cannot set the DEFAULT_DATABASE property of the login so it always will be [master] database.)*/
 CREATE USER xxx FROM LOGIN xxx;
 /*3: Create SQL User on the user database (connect with admin account to user database)*/
